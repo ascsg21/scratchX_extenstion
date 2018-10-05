5b92c27cf4f14194fb87dbaf1f73f48b3512b87e
ascsg21 <lovelyroom77@gmail.com>
2018-10-05 21:14:54 +0900

    test

--- scratchX_test.js
+++ scratchX_test.js
@@ -16,7 +16,7 @@
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
-            ['r', 'aaa %s  bb', 'getImageDescription', ''],
+            ['r', 'image path : %s', 'getImageDescription', ''],
         ],
         displayName: 'google Vision API Block'
     };
